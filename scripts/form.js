dce28c88f431ac981d1efc8e3bce0d29866e8650
SarahMecham <sarahdmecham@gmail.com>
2025-10-02 14:52:05 -0600

    Week05 From Assignment complete

---
+++ scripts/form.js
@@ -0,0 +1,50 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+const productSelect = document.getElementById("product");
+
+if (productSelect) {
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.name;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+  });
+}
+
+const counterDisplay = document.getElementById("reviewCounter");
+if (counterDisplay) {
+  let reviewCount = localStorage.getItem("reviewCount") || 0;
+  reviewCount = Number(reviewCount) + 1;
+  localStorage.setItem("reviewCount", reviewCount);
+
+  counterDisplay.textContent = `You have submitted ${reviewCount} review(s).`;
+}

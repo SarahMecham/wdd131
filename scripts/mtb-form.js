43a241492e76085b9f8aaf712b2b858e0ce6478a
SarahMecham <sarahdmecham@gmail.com>
2025-10-09 16:12:18 -0600

    Form Layout shift fixed

--- scripts/mtb-form.js
+++ scripts/mtb-form.js
@@ -1,6 +1,10 @@
-document.querySelector("#newsletter-form form").addEventListener("submit", function(e) {
-  e.preventDefault();
+const formContainer = document.querySelector(".form-content");
+const thankYou = document.querySelector(".thank-you");
 
-  this.style.display = "none";
-  document.querySelector("#newsletter-form .thank-you").style.display = "block";
-});
+if (formContainer && thankYou) {
+  formContainer.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    formContainer.classList.add("hidden");
+    thankYou.classList.add("show");
+  });
+}
